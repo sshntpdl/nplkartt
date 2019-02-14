32562a90d6b927e41d22b1166cff367c3567160b
Sushant <sshntpdl@gmail.com>
2019-02-14 22:14:46 +0545

    Categories Form Created

--- app/Product.php
+++ app/Product.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Category;
 
 class Product extends Model
 {
@@ -11,5 +12,8 @@ class Product extends Model
     protected $guarded=[];
     
     protected $dates = ['deleted_at'];
-    //
+    
+    public function categories(){
+        return $this->belongsToMany("App\Category");
+    }
 }
